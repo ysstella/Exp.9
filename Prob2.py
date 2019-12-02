3a383f2b3d14589dc602b050c910a23cf398eb06
ysstella <47804212+ysstella@users.noreply.github.com>
2019-12-02 22:09:32 +0800

    Add files via upload

---
+++ Prob2.py
@@ -0,0 +1,7 @@
+import pandas as pd
+data={'Box':['Box1','Box1','Box1','Box2','Box2','Box2'],
+'Dimension':['Length','Width','Height','Length','Width','Height'],
+'Value':[6,4,2,5,3,4]}
+df=pd.DataFrame(data)
+messy=df.pivot(index='Box', columns='Dimension', values='Value')
+messy["Volume"]=messy.Length*messy.Width*messy.Height
